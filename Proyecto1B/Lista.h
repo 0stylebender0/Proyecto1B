8e1093759fd322c083d7a84d87b893af57636c38
666borz666 <iloveass3333@gmail.com>
2024-04-17 10:47:23 -0600

    Archivos de encabezado para las 4 estructuras a usar (vacíos)

---
+++ Proyecto1B/Lista.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <cctype> 
+#include <cstdlib>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <windows.h>
+#include <stdlib.h> 
+#include <math.h>
+#include <sstream>
+#include <random>
+
+using namespace std;
